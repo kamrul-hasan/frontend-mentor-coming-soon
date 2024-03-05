6a046e83886adb72822e924708b11aa22d13f4e7
Kamrul Hasan <k.hasan4997@gmail.com>
2024-03-06 02:38:14 +0600

    updated

---
+++ script.js
@@ -0,0 +1,38 @@
+const emailInput = document.querySelector('#email');
+const btnClick = document.querySelector('#submit');
+const RegexEmail = /\S+@\S+\.\S+/;
+const emailError = document.querySelector('.icon-error-place');
+const emailTextError = document.querySelector('.text-error-place');
+//const fromGroupEl = document.querySelector('.form-group');
+let errors = [];
+
+btnClick.addEventListener('click', e => {
+  // delete the success class after validated
+  emailTextError.classList.remove('text-success-place');
+
+  // reset all errors
+  errors = [];
+  // prevent default form action
+  e.preventDefault();
+  // email address validation
+  const email = emailInput.value;
+
+  if (email == '' || email === undefined) {
+    errors.push('Enter your email');
+    // display error
+    emailError.style.display = 'block';
+    emailTextError.innerText = errors[0];
+  } else if (!email.match(RegexEmail)) {
+    errors.push('Enter your valid email');
+    // display error icon
+    emailError.style.display = 'block';
+    emailTextError.innerText = errors[0];
+  }
+
+  if (!errors.length > 0) {
+    // hide error icon
+    emailError.style.display = 'none';
+    emailTextError.classList.add('text-success-place');
+    emailTextError.innerText = 'Thanks for subscribing to our newsletter.';
+  }
+});
